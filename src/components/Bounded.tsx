9b5723746213651f1866b1cf6e7a5d436e69474c
backendbro <ukagha.nzubechukwu@yahoo.com>
2025-05-09 13:53:42 +0600

    Done

--- src/components/Bounded.tsx
+++ src/components/Bounded.tsx
@@ -1,18 +1,53 @@
 import React from "react";
 import clsx from "clsx";
 
-type BoundedProps = {
-  as?: React.ElementType;
-  className?: string;
-  children: React.ReactNode;
+type AsProp<C extends React.ElementType> = {
+  /** The element or component to render as */
+  as?: C;
 };
 
-const Bounded = React.forwardRef<HTMLDivElement, BoundedProps>(
-  ({ as: Comp = "section", className = "", children, ...restProps }, ref) => {
+type PropsToOmit<C extends React.ElementType, P> = keyof (AsProp<C> & P);
+
+/**
+ * Build the props for a polymorphic component:
+ *  - Your own props (P) plus `as`, plus
+ *  - all props of C, *except* the ones you’re overriding (`as`, `children`, etc.)
+ */
+type PolymorphicProps<C extends React.ElementType, P> = React.PropsWithChildren<
+  P & AsProp<C>
+> &
+  Omit<React.ComponentPropsWithoutRef<C>, PropsToOmit<C, P>>;
+
+type BoundedOwnProps = {
+  className?: string;
+};
+
+/**
+ * Our final props:
+ *  - can pass `as="a"` or `as={MyComponent}`
+ *  - can pass `className`
+ *  - will get full intrinsic props of the chosen `as` *minus* `as`/`children`
+ *  - children is exactly ReactNode
+ */
+type BoundedProps<C extends React.ElementType = "section"> = PolymorphicProps<
+  C,
+  BoundedOwnProps
+>;
+
+const Bounded = React.forwardRef(
+  <C extends React.ElementType = "section">(
+    props: BoundedProps<C>,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    ref: React.Ref<any>
+  ) => {
+    const { as, className, children, ...restProps } = props;
+
+    const Comp = as ?? "section";
+
     return (
       <Comp
         ref={ref}
-        className={clsx(`px-4 py-10 md:px-6 md:py-14 lg:py-16 ${className}`)}
+        className={clsx("px-4 py-10 md:px-6 md:py-14 lg:py-16", className)}
         {...restProps}
       >
         <div className="mx-auto w-full max-w-7xl">{children}</div>
@@ -22,4 +57,5 @@ const Bounded = React.forwardRef<HTMLDivElement, BoundedProps>(
 );
 
 Bounded.displayName = "Bounded";
+
 export default Bounded;
