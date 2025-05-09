c3fc43a635e89714cf6555c054fe19d53785f4c4
backendbro <ukagha.nzubechukwu@yahoo.com>
2025-05-09 15:11:33 +0600

    Done

--- src/components/Bounded.tsx
+++ src/components/Bounded.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import clsx from "clsx";
 
 type BoundedProps = {
-  as?: React.ElementType<{ children?: React.ReactNode }>;
+  as?: React.ElementType;
   className?: string;
   children: React.ReactNode;
 };
@@ -10,6 +10,8 @@ type BoundedProps = {
 const Bounded = React.forwardRef<HTMLDivElement, BoundedProps>(
   ({ as: Comp = "section", className, children, ...restProps }, ref) => {
     return (
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
       <Comp
         ref={ref}
         className={clsx("px-4 py-10 md:px-6 md:py-14 lg:py-16", className)}
